2f030d5fc63c345a9dd4018a5b42551b6b1fd8af
Diiiya <33103806+Diiiya@users.noreply.github.com>
2019-01-07 12:42:54 +0200

    Add project files.

---
+++ RollOfTheDiceRESTConsumer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace RollOfTheDiceRESTConsumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
